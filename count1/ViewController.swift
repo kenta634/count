a23209a0396a46c92de95aeaa52d1672351643a3
高橋健太 <kenta@takahashikenta-no-MacBook-Pro.local>
2015-06-06 15:56:50 +0900

    first commit

---
+++ count1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  count1
+//
+//  Created by 高橋健太 on 2015/06/06.
+//  Copyright (c) 2015年 高橋健太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var number : Int = 0
+    @IBOutlet var label : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+
+
+}
+
